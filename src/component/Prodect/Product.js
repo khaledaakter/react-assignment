d2666beffb6b470295b1855d6ffd95a65b1fd6f6
Khaleda Nahar <khaledanahar43@gmail.com>
2022-03-29 09:15:18 +0600

    .

--- src/component/Prodect/Product.js
+++ src/component/Prodect/Product.js
@@ -1,11 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
+
 import './Product.css';
+
+
 function Product(props) {
     
     if(props.product) {
         const {product, handleAddToCart} = props;
-        const { id, name, price, img } = props.product;
-
+        const { id, name, price, img } = product;
+        
+        
         return (
 
             <div className='product'>
@@ -15,6 +21,7 @@ function Product(props) {
                     <p className='product-price'>Price: ${price}</p>      
                     <button onClick={() => handleAddToCart(product)} className='btn-cart'>
                         <p className='btn-text'>Add to Cart</p>
+                        <FontAwesomeIcon icon={faShoppingCart} />
                     </button>              
                 </div>
             
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react'
 import Product from '../Prodect/Product';
+import "./Shop.css";
 
 function Shop() {
     const [products, setProducts] = useState([]);
+    const [cart, setcart] = useState([]);
 
     useEffect( () => {
         fetch('Products.json')
@@ -11,12 +13,32 @@ function Shop() {
     }, []);
 
 
+    
+        const handleAddToCart = (product) => {
+            console.log(product);
+            if(cart.length < 4) {
+                const newCart = [...cart, product];
+                setcart(newCart);
+            }
+            else {
+                alert("You have alrady 4 item select.");
+            }
+        }
+    
+    // const handleAddToCart = (product) => {
+    //     console.log(product);
+    //     const newCart = [...cart, product];
+    //     setcart(newCart);
+    // }
+
+
+
     return (
         <div>
             <div className='shop-container'>
                 <div className="products-container">
                 {
-                    products.map(product=><Product key={product.id} product={product}
+                    products.map(product=><Product key={product.id} product={product} handleAddToCart={handleAddToCart}
                     />)
                 }
 
@@ -24,7 +46,11 @@ function Shop() {
                 <div className="cart-container">
                     <div className="cart-container-inner">
                         <h4>Order Summary</h4>
-                        <p>Selected Items:</p>
+                        <p>Selected Items: {cart.length}</p>
+                        <div className='addToProduct'></div>
+
+                        <button className='btn-cart btn-select-rendom'><p className='btn-text'>Select Rendom</p></button>   
+                        <button className='btn-cart btn-clear'><p className='btn-text'>Clear</p></button>   
                     </div>
                 </div>
             </div>
